82ab41ae4eb690c07e6c2228f3abe1edb20aff6b
sibouras <sibouras@users.noreply.github.com>
2024-08-24 17:53:36 +0200

    create and fill the grid

--- src/main.js
+++ src/main.js
@@ -1,11 +1,27 @@
-const btn = document.querySelector('#counter')
-let counter = 0
+/** @type {HTMLDivElement} */
+const gridContainer = document.querySelector('.grid')
 
-function setCounter(count) {
-  counter = count
-  btn.innerHTML = `count is ${counter}`
+function fillGrid(size) {
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement('div')
+    gridElement.classList.add('grid-element')
+    // gridElement.addEventListener('click', (e) => {
+    //   /** @type {HTMLDivElement} */
+    //   const target = e.target
+    //   target.style.backgroundColor = 'red'
+    // })
+    gridContainer.append(gridElement)
+  }
 }
 
-btn.addEventListener('click', () => {
-  setCounter(counter + 1)
+fillGrid(8)
+
+gridContainer.addEventListener('click', (e) => {
+  if (
+    e.target instanceof HTMLDivElement &&
+    e.target.classList.contains('grid-element')
+  ) {
+    e.target.style.backgroundColor = 'blue'
+  }
 })
