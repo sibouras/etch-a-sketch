96fb235057c1018044584c32f5bd5b5a967aaed8
sibouras <sibouras@users.noreply.github.com>
2024-08-25 11:12:30 +0200

    only fill elements when mouse is down

--- src/main.js
+++ src/main.js
@@ -6,21 +6,35 @@ function fillGrid(size) {
   for (let i = 0; i < size * size; i++) {
     const gridElement = document.createElement('div')
     gridElement.classList.add('grid-element')
-    // gridElement.addEventListener('click', (e) => {
-    //   /** @type {HTMLDivElement} */
-    //   const target = e.target
-    //   target.style.backgroundColor = 'red'
-    // })
     gridContainer.append(gridElement)
   }
 }
 
-fillGrid(8)
+fillGrid(20)
 
-gridContainer.addEventListener('click', (e) => {
+// using mouseover + mousedown is better than using mousemove.
+// mousemove event fires many times per second as the mouse is moved around, even
+// if it is just by one pixel.
+// also we use preventDefault() on the drag/drop operation in mousedown,
+// or we can add `user-select: none` in css(both fix the element drag).
+
+gridContainer.addEventListener('mouseover', (e) => {
+  if (
+    e.target instanceof HTMLDivElement && // this gives lsp completion to e.target
+    e.target.classList.contains('grid-element') &&
+    e.buttons === 1 // 1 means left button is pressed
+  ) {
+    e.target.style.backgroundColor = 'blue'
+  }
+})
+
+gridContainer.addEventListener('mousedown', (e) => {
+  // prevent drag/drop operation: https://w3c.github.io/uievents/#event-type-mousedown
+  // e.preventDefault() // not using this because im using `user-select:none` in css
   if (
     e.target instanceof HTMLDivElement &&
-    e.target.classList.contains('grid-element')
+    e.target.classList.contains('grid-element') &&
+    e.buttons === 1
   ) {
     e.target.style.backgroundColor = 'blue'
   }
